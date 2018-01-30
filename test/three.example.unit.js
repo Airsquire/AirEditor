c34fe55bf4319ae8f5d8ef4c2f6123029e911fcb
You Yue <youy0004123@gmail.com>
2018-01-31 00:56:17 +0100

    1. Clean folder and package.json 2.Change Tile

--- test/three.example.unit.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author TristanVALCKE / https://github.com/Itee
- */
-
-import './unit/example/exporters/GLTFExporter.tests';
-import './unit/example/loaders/GLTFLoader.tests';
-import './unit/example/objects/LensFlare.tests';
